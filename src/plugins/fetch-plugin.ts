3d56ea5c382b2cdbe54d1aafbff7135f6ca7b91b
p.labib <p.labib@ditech.ae>
2023-02-14 16:18:34 +0200

    loading css files

--- src/plugins/fetch-plugin.ts
+++ src/plugins/fetch-plugin.ts
@@ -1,7 +1,6 @@
 import * as esbuild from "esbuild-wasm";
 import axios from "axios";
 import localForage from "localforage";
-
 interface funcProp {
   inputCode: string;
 }
@@ -15,19 +14,42 @@ export const fetchPlugin = ({ inputCode }: funcProp) => {
     name: "fetch-plugin",
     setup(build: esbuild.PluginBuild) {
       build.onLoad({ filter: /.*/ }, async (args: esbuild.OnLoadArgs) => {
-        if (args.path === "index.js") {
-          return {
-            loader: "jsx",
-            contents: inputCode,
-          };
-        }
-
         const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
           args.path
         );
 
         if (cachedResult) return cachedResult;
+      });
 
+      build.onLoad({ filter: /.css$/ }, async (args: esbuild.OnLoadArgs) => {
+        const { data, request } = await axios.get(args.path);
+        const escape = data
+          .replace(/\n/g, "")
+          .replace(/"/g, '\\"')
+          .replace(/'/g, "\\'");
+        const contents = `
+            const styleElement = document.createElemnt('style');
+            styleElement.innerText = '${escape}';
+            document.head.appendChild(styleElement); `;
+
+        const result: esbuild.OnLoadResult = {
+          loader: "jsx",
+          contents,
+          resolveDir: new URL("./", request.responseURL).pathname,
+        };
+        await fileCache.setItem(args.path, result);
+
+        return result;
+      });
+
+      build.onLoad({ filter: /(^index\.js$)/ }, (args: esbuild.OnLoadArgs) => {
+        return {
+          loader: "jsx",
+          contents: inputCode,
+        };
+      });
+
+      build.onLoad({ filter: /.*/ }, async (args: esbuild.OnLoadArgs) => {
         const { data, request } = await axios.get(args.path);
 
         const result: esbuild.OnLoadResult = {
